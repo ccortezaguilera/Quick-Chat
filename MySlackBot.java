020b14d50017b2c71f29dce6e56c411483a06f5d
ccortezaguilera <ccortezaguilera@dons.usfca.edu>
2017-05-30 22:14:54 -0700

    remove token

--- MySlackBot.java
+++ MySlackBot.java
@@ -2,7 +2,7 @@ import java.util.Map;
 
 /**
  * Created by Carlos on 1/25/2017.
- * Slack test token xoxp-120869210337-131141738960-132633104067-5103018a9f3ad2e2b298b1ec3a79c897
+ *
  */
 public class MySlackBot {
     public static void main(String[] args) {
